050479224aeeb8a2fab7aa475ecf861dafb7da07
Waseem Samkari <wsamkari2022@my.fit.edu>
2026-02-06 18:27:38 -0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/VRDS_APA/',
+  server: {
+    port: 3003,
+    proxy: {
+      '/VRDS_APA/api': {
+        target: 'http://localhost:4003',
+        changeOrigin: true,
+      },
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
